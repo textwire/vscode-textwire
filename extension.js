8f9f91dd44cafa4a962167e62d137c292f9499d2
SerhiiCho <serhiicho@protonmail.com>
2024-03-17 17:52:20 +0200

    Add extension.js file

---
+++ extension.js
@@ -0,0 +1,14 @@
+const vscode = require('vscode')
+
+/**
+ * @param {vscode.ExtensionContext} ctx
+ */
+function activate(ctx) {
+    //
+}
+
+exports.activate = activate
+
+module.exports = {
+    activate,
+}
