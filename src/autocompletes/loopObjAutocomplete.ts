61fa33ba0d6aaba91c5e5a4170f5ea8af6be1705
SerhiiCho <serhiicho@protonmail.com>
2025-01-28 11:08:17 +0200

    Add scope for loop autocomplete only within a loop

--- src/autocompletes/loopObjAutocomplete.ts
+++ src/autocompletes/loopObjAutocomplete.ts
@@ -1,42 +1,65 @@
-import * as vs from 'vscode'
-import completionItem from '@/modules/completionItem'
+import * as vscode from 'vscode'
+import completionItem from '../modules/completionItem'
 
-export default vs.languages.registerCompletionItemProvider(
+function isWithinLoop(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+): boolean {
+    const textBeforeCursor = document.getText(
+        new vscode.Range(new vscode.Position(0, 0), position),
+    )
+
+    // Find all @each and @end locations
+    const eachMatches = Array.from(textBeforeCursor.matchAll(/@(each|for)/g))
+    const endMatches = Array.from(textBeforeCursor.matchAll(/@end/g))
+
+    // Determine if the cursor is within the last @each block
+    const lastEach = eachMatches.pop() || false
+    const lastEnd = endMatches.pop()
+
+    return lastEach && (!lastEnd || lastEach.index > lastEnd.index)
+}
+
+export default vscode.languages.registerCompletionItemProvider(
     { language: 'textwire' },
     {
         provideCompletionItems(
-            document: vs.TextDocument,
-            position: vs.Position,
-        ): vs.CompletionItem[] {
-            const range = new vs.Range(position.with(undefined, 0), position)
-            const textBefore = document.getText(range)
+            document: vscode.TextDocument,
+            position: vscode.Position,
+        ): vscode.CompletionItem[] {
+            if (isWithinLoop(document, position)) {
+                const range = new vscode.Range(position.with(undefined, 0), position)
+                const textBefore = document.getText(range)
 
-            if (!textBefore.endsWith('{{ loop.')) {
-                return []
+                if (!/{{\s*loop\.$/.test(textBefore.trim())) {
+                    return []
+                }
+
+                return [
+                    completionItem(
+                        'index',
+                        '[int] The current iteration of the loop. Starts with 0',
+                        vscode.CompletionItemKind.Field,
+                    ),
+                    completionItem(
+                        'first',
+                        '[bool] Returns true if this is the first iteration',
+                        vscode.CompletionItemKind.Field,
+                    ),
+                    completionItem(
+                        'last',
+                        '[bool] Returns true if this is the last iteration',
+                        vscode.CompletionItemKind.Field,
+                    ),
+                    completionItem(
+                        'iter',
+                        '[int] The current iteration of the loop. Starts with 1',
+                        vscode.CompletionItemKind.Field,
+                    ),
+                ]
             }
 
-            return [
-                completionItem(
-                    'index',
-                    '[int] The current iteration of the loop. Starts with 0',
-                    vs.CompletionItemKind.Field,
-                ),
-                completionItem(
-                    'first',
-                    '[bool] Returns true if this is the first iteration',
-                    vs.CompletionItemKind.Field,
-                ),
-                completionItem(
-                    'last',
-                    '[bool] Returns true if this is the last iteration',
-                    vs.CompletionItemKind.Field,
-                ),
-                completionItem(
-                    'iter',
-                    '[int] The current iteration of the loop. Starts with 1',
-                    vs.CompletionItemKind.Field,
-                ),
-            ]
+            return []
         },
     },
     '{',
