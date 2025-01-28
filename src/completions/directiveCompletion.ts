8371ede0a79d73b23addc21ded33773fc7bc2b17
SerhiiCho <serhiicho@protonmail.com>
2025-01-28 19:37:00 +0200

    Refactor directiveCompletion

--- src/completions/directiveCompletion.ts
+++ src/completions/directiveCompletion.ts
@@ -1,103 +1,9 @@
 import * as vscode from 'vscode'
 import completionItem from '../modules/completionItem'
+import directivesInfo from '../static/info/directivesInfo'
 
 const DIR_START_REG = /(?<!\\)@(\w*)$/
 const triggerChars = ['@']
-
-const IF_DESC = `(directive)
-Conditionally render content
-
-\`\`\`textwire
-@if(true)
-    content
-@end
-\`\`\``
-
-const IF_ELSE_DESC = `(directive)
-Conditionally render content and provide an alternative that will be rendered when the condition is false
-
-\`\`\`textwire
-@if(false)
-    content
-@else
-    alternative content
-@end
-\`\`\``
-
-const EACH_DESC = `(directive)
-Loop that iterates over arrays
-
-\`\`\`textwire
-@each(item in items)
-    {{ item }}
-@end
-\`\`\``
-
-const DUMP_DESC = `(directive)
-Debugging helper to output the value of variables
-
-\`\`\`textwire
-@dump(variable)
-\`\`\`
-
-Outputs the value of variables, objects, arrays, strings, and other data types to the screen.`
-
-const USE_DESC = `(directive)
-Specify the layout file to be used for rendering the current template
-
-\`\`\`textwire
-@use('layoutName')
-\`\`\`
-
-This directive includes the layout file specified, which defines the overall structure of the page.`
-
-const INSERT_DESC = `(directive)
-Inject content into reserved placeholders defined in the layout file by providing a second argument as content
-
-\`\`\`textwire
-@insert('reservedName', 'content')
-\`\`\`
-
-Use this directive to specify content for placeholders in the layout file.`
-
-const INSERT_END_DESC = `(directive)
-Inject content into reserved placeholders defined in the layout file by providing a block of content
-
-\`\`\`textwire
-@insert('reservedName')
-    content
-@end
-\`\`\`
-
-Use this directive to specify content for placeholders in the layout file.`
-
-const EACH_ELSE_DESC = `(directive)
-Loop that iterates over arrays, with a fallback for empty arrays
-
-\`\`\`textwire
-@each(item in items)
-    {{ item }}
-@else
-    No items available.
-@end
-\`\`\`
-
-Use the @else directive to specify content that will be displayed if the array is empty.`
-
-const IF_ELSEIF_DESC = `(directive)
-Conditionally render content with additional conditions using @elseif
-
-\`\`\`textwire
-@if(condition1)
-    content for condition1
-@elseif(condition2)
-    content for condition2
-@else
-    content when all conditions are false
-@end
-\`\`\`
-
-Use the @elseif directive to handle additional conditional branches. If none of the conditions are met, use @else to provide fallback content.`
 
 export default vscode.languages.registerCompletionItemProvider(
     { language: 'textwire' },
@@ -120,55 +26,55 @@ export default vscode.languages.registerCompletionItemProvider(
             const dirs = [
                 completionItem(
                     '@if',
-                    IF_DESC,
+                    directivesInfo.if,
                     vscode.CompletionItemKind.Function,
                     'if($1)\n\n@end',
                 ),
                 completionItem(
                     '@if @else',
-                    IF_ELSE_DESC,
+                    directivesInfo.ifElse,
                     vscode.CompletionItemKind.Function,
                     'if($1)\n\n@else\n\n@end',
                 ),
                 completionItem(
                     '@if @elseif',
-                    IF_ELSEIF_DESC,
+                    directivesInfo.ifElseif,
                     vscode.CompletionItemKind.Function,
                     'if($1)\n\n@elseif($2)\n\n@end',
                 ),
                 completionItem(
                     '@use',
-                    USE_DESC,
+                    directivesInfo.use,
                     vscode.CompletionItemKind.Function,
                     'use($1)',
                 ),
                 completionItem(
                     '@insert',
-                    INSERT_DESC,
+                    directivesInfo.insert,
                     vscode.CompletionItemKind.Function,
                     'insert($1)',
                 ),
                 completionItem(
                     '@insert @end',
-                    INSERT_END_DESC,
+                    directivesInfo.insertEnd,
                     vscode.CompletionItemKind.Function,
                     'insert($1)\n    $2\n@end',
                 ),
                 completionItem(
                     '@each',
-                    EACH_DESC,
+                    directivesInfo.each,
                     vscode.CompletionItemKind.Function,
                     'each($1 in $2)\n    $2\n@end',
                 ),
                 completionItem(
                     '@each @else',
-                    EACH_ELSE_DESC,
+                    directivesInfo.eachElse,
                     vscode.CompletionItemKind.Function,
                     'each($1 in $2)\n    $2\n@else\n    $2\n@end',
                 ),
                 completionItem(
                     '@dump',
-                    DUMP_DESC,
+                    directivesInfo.dump,
                     vscode.CompletionItemKind.Function,
                     'dump($1)',
                 ),
---
+++ src/static/info/directivesInfo.ts
@@ -0,0 +1,96 @@
+export default {
+    if: `(directive)
+Conditionally render content
+
+\`\`\`textwire
+@if(condition)
+    <p>content</p>
+@end
+\`\`\``,
+
+    ifElse: `(directive)
+Conditionally render content and provide an alternative that will be rendered when the condition is false
+
+\`\`\`textwire
+@if(condition)
+    <p>content</p>
+@else
+    <p>alternative content</p>
+@end
+\`\`\``,
+
+    each: `(directive)
+Loop that iterates over arrays
+
+\`\`\`textwire
+@each(item in items)
+    <p>{{ item }}</p>
+@end
+\`\`\``,
+
+    dump: `(directive)
+Debugging helper to output the value of variables
+
+\`\`\`textwire
+@dump(variable)
+\`\`\`
+
+Outputs the value of variables, objects, arrays, strings, and other data types to the screen.`,
+
+    use: `(directive)
+Specify the layout file to be used for rendering the current template
+
+\`\`\`textwire
+@use('layoutName')
+\`\`\`
+
+This directive includes the layout file specified, which defines the overall structure of the page.`,
+
+    insert: `(directive)
+Inject content into reserved placeholders defined in the layout file by providing a second argument as content
+
+\`\`\`textwire
+@insert('reservedName', 'content')
+\`\`\`
+
+Use this directive to specify content for placeholders in the layout file.`,
+
+    insertEnd: `(directive)
+Inject content into reserved placeholders defined in the layout file by providing a block of content
+
+\`\`\`textwire
+@insert('reservedName')
+    <p>content</p>
+@end
+\`\`\`
+
+Use this directive to specify content for placeholders in the layout file.`,
+
+    eachElse: `(directive)
+Loop that iterates over arrays, with a fallback for empty arrays
+
+\`\`\`textwire
+@each(item in items)
+    {{ item }}
+@else
+    <p>No items available.</p>
+@end
+\`\`\`
+
+Use the @else directive to specify content that will be displayed if the array is empty.`,
+
+    ifElseif: `(directive)
+Conditionally render content with additional conditions using @elseif
+
+\`\`\`textwire
+@if(condition1)
+    <p>content</p> for condition1
+@elseif(condition2)
+    <p>content for condition2</p>
+@else
+    <p>content when all conditions are false</p>
+@end
+\`\`\`
+
+Use the @elseif directive to handle additional conditional branches. If none of the conditions are met, use @else to provide fallback content.`,
+}
