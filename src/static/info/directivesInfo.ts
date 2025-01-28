066277de5838fa79b105b0f902639bb87edef4c0
SerhiiCho <serhiicho@protonmail.com>
2025-01-28 19:44:55 +0200

    Improve descriptions of directive

--- src/static/info/directivesInfo.ts
+++ src/static/info/directivesInfo.ts
@@ -1,6 +1,6 @@
 export default {
     if: `(directive)
-Conditionally render content
+Conditionally render content.
 
 \`\`\`textwire
 @if(condition)
@@ -9,7 +9,7 @@ Conditionally render content
 \`\`\``,
 
     ifElse: `(directive)
-Conditionally render content and provide an alternative that will be rendered when the condition is false
+Conditionally render content and provide an alternative that will be rendered when the condition is \`false\`.
 
 \`\`\`textwire
 @if(condition)
@@ -20,7 +20,7 @@ Conditionally render content and provide an alternative that will be rendered wh
 \`\`\``,
 
     each: `(directive)
-Loop that iterates over arrays
+Loop that iterates over arrays.
 
 \`\`\`textwire
 @each(item in items)
@@ -28,8 +28,21 @@ Loop that iterates over arrays
 @end
 \`\`\``,
 
+    eachElse: `(directive)
+Loop that iterates over arrays, with a fallback for empty arrays.
+
+\`\`\`textwire
+@each(item in items)
+    <p>{{ item }}</p>
+@else
+    <p>No items available.</p>
+@end
+\`\`\`
+
+Use the @else directive to specify content that will be displayed if the array is empty.`,
+
     dump: `(directive)
-Debugging helper to output the value of variables
+Debugging helper to output the value of variables.
 
 \`\`\`textwire
 @dump(variable)
@@ -38,7 +51,7 @@ Debugging helper to output the value of variables
 Outputs the value of variables, objects, arrays, strings, and other data types to the screen.`,
 
     use: `(directive)
-Specify the layout file to be used for rendering the current template
+Specify the layout file to be used for rendering the current template.
 
 \`\`\`textwire
 @use('layoutName')
@@ -47,7 +60,7 @@ Specify the layout file to be used for rendering the current template
 This directive includes the layout file specified, which defines the overall structure of the page.`,
 
     insert: `(directive)
-Inject content into reserved placeholders defined in the layout file by providing a second argument as content
+Inject content into reserved placeholders defined in the layout file by providing a second argument as content.
 
 \`\`\`textwire
 @insert('reservedName', 'content')
@@ -56,7 +69,7 @@ Inject content into reserved placeholders defined in the layout file by providin
 Use this directive to specify content for placeholders in the layout file.`,
 
     insertEnd: `(directive)
-Inject content into reserved placeholders defined in the layout file by providing a block of content
+Inject content into reserved placeholders defined in the layout file by providing a block of content.
 
 \`\`\`textwire
 @insert('reservedName')
@@ -66,31 +79,18 @@ Inject content into reserved placeholders defined in the layout file by providin
 
 Use this directive to specify content for placeholders in the layout file.`,
 
-    eachElse: `(directive)
-Loop that iterates over arrays, with a fallback for empty arrays
-
-\`\`\`textwire
-@each(item in items)
-    {{ item }}
-@else
-    <p>No items available.</p>
-@end
-\`\`\`
-
-Use the @else directive to specify content that will be displayed if the array is empty.`,
-
     ifElseif: `(directive)
-Conditionally render content with additional conditions using @elseif
+Conditionally render content with additional conditions using \`@elseif\`.
 
 \`\`\`textwire
 @if(condition1)
-    <p>content</p> for condition1
+    <p>condition1 is true</p>
 @elseif(condition2)
-    <p>content for condition2</p>
+    <p>condition2 is true</p>
 @else
-    <p>content when all conditions are false</p>
+    <p>when all false</p>
 @end
 \`\`\`
 
-Use the @elseif directive to handle additional conditional branches. If none of the conditions are met, use @else to provide fallback content.`,
+Use the \`@elseif\` directive to handle additional conditional branches. If none of the conditions are met, use @else to provide fallback content.`,
 }
