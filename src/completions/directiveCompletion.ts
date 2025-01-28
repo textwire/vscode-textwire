62699bc1fdec48aea37ea50b763e137f79897c80
SerhiiCho <serhiicho@protonmail.com>
2025-01-28 15:15:07 +0200

    WIP more directives to suggestions

--- src/completions/directiveCompletion.ts
+++ src/completions/directiveCompletion.ts
@@ -24,18 +24,45 @@ export default vscode.languages.registerCompletionItemProvider(
 
             const dirs = [
                 completionItem(
-                    '@dump()',
-                    '(directive) @dump: str\n\n' +
+                    '@if',
+                    '(directive) @if\n\nConditionally render content',
+                    vscode.CompletionItemKind.Function,
+                    'if($1)\n\n@end',
+                ),
+                completionItem(
+                    '@if @else',
+                    '(directive) @if\n\nConditionally render content. ' +
+                        'The @else directive is used to display content when the condition is false',
+                    vscode.CompletionItemKind.Function,
+                    'if($1)\n\n@else\n\n@end',
+                ),
+                completionItem(
+                    '@if @elseif',
+                    '(directive) @if\n\nConditionally render content. ' +
+                        'The @elseif directive is used to display content when the condition is false',
+                    vscode.CompletionItemKind.Function,
+                    'if($1)\n\n@elseif($2)\n\n@end',
+                ),
+                completionItem(
+                    '@each',
+                    '(directive) @each\n\nLoop that iterates over arrays',
+                    vscode.CompletionItemKind.Function,
+                    'each($1 in $2)\n\n@end',
+                ),
+                completionItem(
+                    '@each @else',
+                    '(directive) @each\n\nLoop that iterates over arrays. ' +
+                        'The @else directive is used to display content when the array is empty',
+                    vscode.CompletionItemKind.Function,
+                    'each($1 in $2)\n\n@else\n\n@end',
+                ),
+                completionItem(
+                    '@dump',
+                    '(directive) @dump\n\n' +
                         'Debugging helper that outputs the value of variables, ' +
                         'objects, arrays, strings and other data types to the screen',
                     vscode.CompletionItemKind.Function,
                     'dump($1)',
-                ),
-                completionItem(
-                    '@each()',
-                    '(directive) @each: loop\n\nLoop that iterates over arrays',
-                    vscode.CompletionItemKind.Function,
-                    'each($1 in $2)\n\n@end',
                 ),
             ]
 
