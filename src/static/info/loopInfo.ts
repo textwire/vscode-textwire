0597d84e74441c9f7b547d4a3dc7489121123722
SerhiiCho <serhiicho@protonmail.com>
2025-01-29 14:44:03 +0200

    Refactor loopInfo.ts

--- src/static/info/loopInfo.ts
+++ src/static/info/loopInfo.ts
@@ -1,29 +1,34 @@
-export default {
-    index: `(property) index: int
+const index = `(property) index: int
 The current iteration of the loop. Starts with 0.
 
 \`\`\`textwire
 {{ loop.index }}
-\`\`\``,
+\`\`\``
 
-    first: `(property) first: bool
+const first = `(property) first: bool
 Returns \`true\` if this is the first iteration of the loop.
 
 \`\`\`textwire
 {{ loop.first }}
-\`\`\``,
+\`\`\``
 
-    last: `(property) last: bool
+const last = `(property) last: bool
 Returns \`true\` if this is the last iteration of the loop.
 
 \`\`\`textwire
 {{ loop.last }}
-\`\`\``,
+\`\`\``
 
-    iter: `(property) iter: int
+const iter = `(property) iter: int
 The current iteration of the loop. Starts with 1.
 
 \`\`\`textwire
 {{ loop.iter }}
-\`\`\``,
+\`\`\``
+
+export default {
+    index,
+    first,
+    last,
+    iter,
 }
