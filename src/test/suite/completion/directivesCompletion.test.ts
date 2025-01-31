019b2295ee78d7048e7981b9549c051e4d24e2de
SerhiiCho <serhiicho@protonmail.com>
2025-01-31 16:13:57 +0200

    Refactor test

---
+++ src/test/suite/completion/directivesCompletion.test.ts
@@ -0,0 +1,10 @@
+import * as assert from 'assert'
+import * as vscode from 'vscode'
+import triggerCompletion from '../utils/triggerCompletion'
+import openTextDocument from '../utils/openTextDocument'
+
+suite('Directives Completion', () => {
+    test('Should suggest directives in HTML', async () => {
+        return
+    })
+})
--- src/test/suite/completion/loopsCompletion.test.ts
+++ src/test/suite/completion/loopsCompletion.test.ts
@@ -1,15 +1,15 @@
 import * as assert from 'assert'
 import * as vscode from 'vscode'
-import triggerAutocomplete from '../utils/triggerAutocomplete'
+import triggerCompletion from '../utils/triggerCompletion'
 import openTextDocument from '../utils/openTextDocument'
 
-suite('Autocomplete - loop variable', () => {
-    test('Should suggest loop properties inside @each', async () => {
+suite('Loops Completion', () => {
+    test('suggests loop properties inside @each', async () => {
         const content = `@each(item in items){{ loop. }}@end`
         const doc = await openTextDocument(content)
         const pos = new vscode.Position(0, 29)
 
-        const completions = await triggerAutocomplete(pos, doc.uri)
+        const completions = await triggerCompletion(pos, doc.uri)
 
         if (!completions) {
             assert.fail('No completions found!')
@@ -25,12 +25,12 @@ suite('Autocomplete - loop variable', () => {
         }
     })
 
-    test('Should not suggest loop properties outside @each', async () => {
+    test('does not suggest loop properties outside @each', async () => {
         const content = `<div>{{ loop. }}</div>@each(item in items){{ item }}@end`
         const doc = await openTextDocument(content)
         const pos = new vscode.Position(0, 12)
 
-        const completions = await triggerAutocomplete(pos, doc.uri)
+        const completions = await triggerCompletion(pos, doc.uri)
 
         if (!completions) {
             assert.fail('No completions found! Should have HTML native completions')
@@ -42,7 +42,12 @@ suite('Autocomplete - loop variable', () => {
 
         for (const expectedLabel of shouldNotHave) {
             assert.ok(
-                !completions.items.some(actualItem => actualItem.label === expectedLabel),
+                !completions.items.some(actualItem => {
+                    return (
+                        actualItem.label === expectedLabel &&
+                        actualItem.kind === vscode.CompletionItemKind.Field
+                    )
+                }),
                 `Unexpected completion '${expectedLabel}' found!`,
             )
         }
