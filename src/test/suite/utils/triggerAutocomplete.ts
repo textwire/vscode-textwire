8928b52baf8be11bf8c72d3907a6e95382e89f9d
SerhiiCho <serhiicho@protonmail.com>
2025-01-31 16:13:28 +0200

    Remove build directory

--- src/test/suite/utils/triggerAutocomplete.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode'
-
-export default async (pos: vscode.Position, uri: vscode.Uri) => {
-    return await vscode.commands.executeCommand<vscode.CompletionList>(
-        'vscode.executeCompletionItemProvider',
-        uri,
-        pos,
-    )
-}
