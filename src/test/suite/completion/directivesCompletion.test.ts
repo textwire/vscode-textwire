dbae6cf246a1bbbbf9238d543dfeb8a37ac327ff
SerhiiCho <serhiicho@protonmail.com>
2025-02-02 11:59:22 +0200

    Add one more test

--- src/test/suite/completion/directivesCompletion.test.ts
+++ src/test/suite/completion/directivesCompletion.test.ts
@@ -39,34 +39,47 @@ suite('Directives Completion', () => {
         }
     })
 
-    test('suggests loop directives inside @each loop', async () => {
-        const content = `<div>@each(item in items) {{ item }} @ @end</div>`
-        const doc = await openTextDocument(content)
-        const pos = new vscode.Position(0, 38)
-        const completions = await triggerCompletion(pos, doc.uri)
+    const loopTests = [
+        {
+            name: '@each',
+            content: `<div>@each(item in items) {{ item }} @ @end</div>`,
+            pos: new vscode.Position(0, 38),
+        },
+        {
+            name: '@for',
+            content: `<div>@for(c = 1; c < items.length(); c++) {{ items[c] }} @ @end</div>`,
+            pos: new vscode.Position(0, 58),
+        },
+    ]
 
-        if (!completions) {
-            assert.fail('No completions found!')
-        }
+    loopTests.forEach(({ name, content, pos }) => {
+        test(`suggests loop directives inside ${name} loop`, async () => {
+            const doc = await openTextDocument(content)
+            const completions = await triggerCompletion(pos, doc.uri)
 
-        assert.ok(completions.hasOwnProperty('items'), 'Property "items" not found!')
+            if (!completions) {
+                assert.fail('No completions found!')
+            }
 
-        assert.strictEqual(
-            completions.items.length,
-            INSIDE_LOOP_DIRECTIVE_NAMES.length,
-            `Expected ${INSIDE_LOOP_DIRECTIVE_NAMES.length} completions, but got ${completions.items.length}`,
-        )
+            assert.ok(completions.hasOwnProperty('items'), 'Property "items" not found!')
 
-        for (const item of completions.items) {
-            assert.ok(
-                INSIDE_LOOP_DIRECTIVE_NAMES.includes(item.label),
-                `Directive ${item.label} not found inside a loop`,
+            assert.strictEqual(
+                completions.items.length,
+                INSIDE_LOOP_DIRECTIVE_NAMES.length,
+                `Expected ${INSIDE_LOOP_DIRECTIVE_NAMES.length} completions, but got ${completions.items.length}`,
             )
 
-            assert.ok(
-                !DIRECTIVE_NAMES.includes(item.label),
-                `Directive ${item.label} should not be suggested inside of a loop`,
-            )
-        }
+            for (const item of completions.items) {
+                assert.ok(
+                    INSIDE_LOOP_DIRECTIVE_NAMES.includes(item.label),
+                    `Directive ${item.label} not found inside a loop`,
+                )
+
+                assert.ok(
+                    !DIRECTIVE_NAMES.includes(item.label),
+                    `Directive ${item.label} should not be suggested inside of a loop`,
+                )
+            }
+        })
     })
 })
