c67ca2f51994ac4ac19e990c5f3effb445ac6d71
SerhiiCho <serhiicho@protonmail.com>
2025-02-02 11:56:01 +0200

    Add test

--- src/test/suite/completion/directivesCompletion.test.ts
+++ src/test/suite/completion/directivesCompletion.test.ts
@@ -2,7 +2,10 @@ import * as assert from 'assert'
 import * as vscode from 'vscode'
 import { triggerCompletion } from '../utils/triggerCompletion'
 import { openTextDocument } from '../utils/openTextDocument'
-import { DIRECTIVE_NAMES } from '../utils/static/directiveNames'
+import {
+    DIRECTIVE_NAMES,
+    INSIDE_LOOP_DIRECTIVES as INSIDE_LOOP_DIRECTIVE_NAMES,
+} from '../utils/static/directiveNames'
 
 suite('Directives Completion', () => {
     test('suggests directives in HTML', async () => {
@@ -26,7 +29,43 @@ suite('Directives Completion', () => {
         for (const item of completions.items) {
             assert.ok(
                 DIRECTIVE_NAMES.includes(item.label),
-                `Unexpected completion found: ${item.label}`,
+                `Directive ${item.label} not found in HTML`,
+            )
+
+            assert.ok(
+                !INSIDE_LOOP_DIRECTIVE_NAMES.includes(item.label),
+                `Directive ${item.label} should not be suggested outside of a loop`,
+            )
+        }
+    })
+
+    test('suggests loop directives inside @each loop', async () => {
+        const content = `<div>@each(item in items) {{ item }} @ @end</div>`
+        const doc = await openTextDocument(content)
+        const pos = new vscode.Position(0, 38)
+        const completions = await triggerCompletion(pos, doc.uri)
+
+        if (!completions) {
+            assert.fail('No completions found!')
+        }
+
+        assert.ok(completions.hasOwnProperty('items'), 'Property "items" not found!')
+
+        assert.strictEqual(
+            completions.items.length,
+            INSIDE_LOOP_DIRECTIVE_NAMES.length,
+            `Expected ${INSIDE_LOOP_DIRECTIVE_NAMES.length} completions, but got ${completions.items.length}`,
+        )
+
+        for (const item of completions.items) {
+            assert.ok(
+                INSIDE_LOOP_DIRECTIVE_NAMES.includes(item.label),
+                `Directive ${item.label} not found inside a loop`,
+            )
+
+            assert.ok(
+                !DIRECTIVE_NAMES.includes(item.label),
+                `Directive ${item.label} should not be suggested inside of a loop`,
             )
         }
     })
