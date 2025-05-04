b5f612f4fb68058529090c9cc8644ccae87f0648
SerhiiCho <serhiicho@protonmail.com>
2025-05-04 11:47:52 +0300

    chore(lsp): wip

--- src/lsp.ts
+++ src/lsp.ts
@@ -1,23 +1,34 @@
+import type { Release, ErrorResponse } from './types/github'
 import * as vscode from 'vscode'
+import { getExtension } from './modules/extension'
+import { logger } from './modules/logger'
+import axios from 'axios'
 
 export async function updateLSP(ctx: vscode.ExtensionContext): Promise<void> {
-    const ext = vscode.extensions.getExtension('SerhiiCho.textwire')
+    const ext = getExtension()
+    const version = ext.packageJSON.lspVersion
+    const currentVersion = ctx.globalState.get<string>('lspVersion')
 
-    if (!ext) {
-        throw new Error('Textwire extension not found')
-    }
+    // TODO: temporarily commented
+    // if (version === currentVersion) {
+    //     return
+    // }
 
-    const version = ext.packageJSON.version
-    const lastVersion = ctx.globalState.get<string>('extensionVersion')
+    await handleUpdate(version)
+    await ctx.globalState.update('lspVersion', version)
+}
 
-    if (lastVersion === version) {
+async function handleUpdate(version: string): Promise<void> {
+    const url = `https://api.github.com/repos/textwire/lsp/releases/tags/v${version}`
+    const resp = await axios.get<Release | ErrorResponse>(url)
+
+    if (resp.status !== 200) {
+        logger.error(
+            'Failed to fetch the latest LSP release. Response status:',
+            resp.status,
+        )
         return
     }
 
-    await handleUpdate()
-    await ctx.globalState.update('extensionVersion', version)
-}
-
-async function handleUpdate(): Promise<void> {
-    // TODO: here
+    const latestRelease = resp.data
 }
