64f9749609fd670fe51346f7c528884b5b51d6ef
SerhiiCho <serhiicho@protonmail.com>
2025-05-06 17:29:39 +0300

    refactor(completions): now they use LSP

--- src/completions/index.ts
+++ src/completions/index.ts
@@ -1,16 +1,11 @@
 import * as vscode from 'vscode'
-import { directivesCompletions } from './directives'
-import { loopObjCompletions } from './loopObj'
 
 export function registerCompletionProvider(): vscode.Disposable {
     return vscode.languages.registerCompletionItemProvider(
         'textwire',
         {
             provideCompletionItems(doc: vscode.TextDocument, pos: vscode.Position) {
-                return [
-                    ...directivesCompletions(doc, pos),
-                    ...loopObjCompletions(doc, pos),
-                ]
+                return []
             },
         },
         '@',
