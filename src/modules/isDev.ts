74c5a5687e0c675c8712b586f84e5c229dd94ef7
SerhiiCho <serhiicho@protonmail.com>
2026-01-13 18:45:35 +0200

    chore: remove tests

--- src/modules/isDev.ts
+++ src/modules/isDev.ts
@@ -1,8 +1,5 @@
 import { ExtensionContext, ExtensionMode } from 'vscode'
 
 export function isDev(ctx: ExtensionContext): boolean {
-    return (
-        ctx.extensionMode === ExtensionMode.Development ||
-        ctx.extensionMode === ExtensionMode.Test
-    )
+    return ctx.extensionMode === ExtensionMode.Development
 }
